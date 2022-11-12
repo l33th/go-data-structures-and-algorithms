108af9edda50c84137da29e35a8715c79d36b8e7
l33th <wmouton.dev@gmail.com>
2022-11-12 06:51:27 +0200

    chore: Complexity

---
+++ Go Chapter 1/complexity/complexity.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// main method
+func main() {
+	var m [10]int
+	var k int
+
+	for k = 0; k < 10; k++ {
+		m[k] = k + 200
+
+		fmt.Printf("Element[%d] = %d\n", k, m[k])
+	}
+}
