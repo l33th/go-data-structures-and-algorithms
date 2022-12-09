447e4f2843cb3a4bbf0881be040a4275ea0cec89
l33th <wmouton.dev@gmail.com>
2022-12-09 20:34:48 +0200

    chore: Num Operations

---
+++ Go Chapter 1/num_operations/num_operations.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+//add method given a and b integers
+func add(a int, b int) int {
+	return a + b
+}
+
+// main method
+func main() {
+	//fmt.Println("My favorite number is", rand.Intn(10))
+	//fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	//fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
+}
